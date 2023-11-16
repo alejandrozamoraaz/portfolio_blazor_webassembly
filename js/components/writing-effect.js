1f31804df80a3c610b9257db0a4fa2e44916e363
alejandrozamoraaz <88607814+alejandrozamoraaz@users.noreply.github.com>
2023-11-16 21:08:16 +0000

    Deploying to portfolio-release from @ alejandrozamoraaz/Portfolio@f27ab88108ab900d52b2b876f3dde392670970e8 🚀

---
+++ js/components/writing-effect.js
@@ -0,0 +1,42 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    window.consoleText = function (words) {
+        const colors = ['#9802cd', '#6960fc', '#dc9af3'];
+        let letterCount = 1;
+        let x = 1;
+        let waiting = false;
+
+        const con = document.getElementById('console');
+        const target = document.getElementById('writing-text');
+
+        target.style.color = colors[0];
+
+        setInterval(function () {
+            if (letterCount === 0 && !waiting) {
+                waiting = true;
+                target.innerHTML = words[0].substring(0, letterCount);
+                setTimeout(function () {
+                    colors.push(colors.shift());
+                    words.push(words.shift());
+                    x = 1;
+                    target.style.color = colors[0];
+                    letterCount += x;
+                    waiting = false;
+                }, 1000);
+            } else if (letterCount === words[0].length + 1 && !waiting) {
+                waiting = true;
+                setTimeout(function () {
+                    x = -1;
+                    letterCount += x;
+                    waiting = false;
+                }, 1000);
+            } else if (!waiting) {
+                target.innerHTML = words[0].substring(0, letterCount);
+                letterCount += x;
+            }
+        }, 120);
+
+        setInterval(function () {
+            con.style.visibility = (con.style.visibility === 'visible') ? 'hidden' : 'visible';
+        }, 400);
+    };
+});
